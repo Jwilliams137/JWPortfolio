2d3fdd4d89d2f58066b4faab0b278fa46f8c9911
Julie Williams <jwilliams137.036@gmail.com>
2025-02-20 20:37:36 -0500

    starting out

---
+++ src/app/contact/page.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './page.module.css' // Importing the styles
+
+function page() {
+  return (
+    <div className={styles.container}>
+      <form action="/api/contact" method="POST" className={styles.form}>
+
+        <input 
+          type="text" 
+          name="name" 
+          placeholder="Your Name" 
+          required 
+          className={styles.inputField} 
+        />
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="Your Email" 
+          required 
+          className={styles.inputField} 
+        />
+        <textarea 
+          name="message" 
+          placeholder="Your Message" 
+          required 
+          className={styles.textarea}
+        ></textarea>
+
+        <input type="hidden" name="_next" value="https://atkinson-art.netlify.app/thank-you" className={styles.hidden} />
+        <input type="hidden" name="_subject" value="New message from your website visitor" className={styles.hidden} />
+        <input type="hidden" name="_honey" className={styles.hidden} />
+
+        <button type="submit" className={styles.button}>Send Message</button>
+      </form>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import styles from './Footer.module.css';
+import footerData from './footerData.json';
+
+const iconMapping = {
+  faFacebook: faGithub
+};
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {footerData.socialLinks.map(({ href, icon }) => (
+          <a
+            key={href}
+            href={href}
+            className={styles.navlink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              className={styles.socialLink}
+              icon={iconMapping[icon]}
+              size="xl"
+            />
+          </a>
+        ))}
+      </div>
+      <span className={styles.copyright}>{footerData.copyright.replace("{year}", new Date().getFullYear())}</span>
+    </footer>
+  );
+}
+
+export default Footer;
